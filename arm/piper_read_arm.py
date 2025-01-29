8d4f29ef6cdf9c675ac433ac4d84992d8072d6bb
Mahi Shafiullah <3000253+notmahi@users.noreply.github.com>
2025-01-29 16:52:19 -0500

    Updated code for joint reading.

--- arm/piper_read_arm.py
+++ arm/piper_read_arm.py
@@ -5,19 +5,28 @@
 from typing import (
     Optional,
 )
-from piper_sdk import *
+from piper_sdk import C_PiperInterface
+import time
+
+joint_limits = {}
 
 # 测试代码
 if __name__ == "__main__":
     piper = C_PiperInterface()
     piper.ConnectPort()
     while True:
-        import time
-
         # a.SearchMotorMaxAngleSpdAccLimit(1, 1)
         # a.ArmParamEnquiryAndConfig(1,0,2,0,3)
         # a.GripperCtrl(50000,1500,0x01)
-        print(piper.GetArmJointMsgs())
+        # print(piper.GetArmJointMsgs())
+        data = piper.GetArmJointMsgs()
+        for line in data.split("\n"):
+            if line.startswith("Joint"):
+                name, value = line.split(":")
+                name = name.split(" ")[1]
+                joint = int(name)
+                value = int(value.split(",")[0])
+                joint_limits[joint] = value
+        print(joint_limits)
         print(piper.GetArmGripperMsgs())
         time.sleep(0.005)
-        pass
