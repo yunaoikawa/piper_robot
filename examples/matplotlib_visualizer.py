81a3ad9a3b33facea126e6a621630452a253ec5b
eneserciyes <m.eneserciyes@gmail.com>
2025-02-04 00:51:51 -0500

    rerun visualizer working!!

---
+++ examples/matplotlib_visualizer.py
@@ -0,0 +1,85 @@
+import zmq
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import math
+from collections import deque
+
+from robot.timer import FrequencyTimer
+from robot.constants import LENGTH, WIDTH
+from robot.communications import ROBOT_IP, STATE_PORT
+
+def record_state():
+    topic = bytes("state ", 'utf-8')
+    state_subscriber = zmq.Context().socket(zmq.SUB)
+    state_subscriber.connect(f"tcp://{ROBOT_IP}:{STATE_PORT}")
+    state_subscriber.setsockopt(zmq.CONFLATE, 1)
+    state_subscriber.setsockopt(zmq.SUBSCRIBE, topic)
+    timer = FrequencyTimer(frequency=10)
+
+    states = []
+    start_time = time.time()
+
+    try:
+        while True:
+            with timer:
+                buffer = state_subscriber.recv().lstrip(topic)
+                if len(buffer) % np.dtype(float).itemsize != 0: continue
+                state = np.frombuffer(buffer, dtype=float)
+
+                states.append(state)
+    except KeyboardInterrupt:
+        print(f"Recording stopped. Duration: {time.time() - start_time} seconds")
+        print(f"Number of states recorded: {len(states)}")
+        np.save("states.npy", np.array(states))
+
+class Visualizer:
+    def __init__(self):
+        # Odometry figure
+        plt.figure('Odometry')
+        plt.axis([-1, 1, -1, 1])
+        self.fig_axis = plt.gca()
+        self.fig_axis.set_aspect('equal')
+        plt.grid(which='both')
+        self.robot_line, = plt.plot([], color='tab:gray')
+        self.robot_arrow = plt.arrow(0, 0, 0, 0, head_width=0)
+        self.odom_line, = plt.plot([], '--', color='tab:blue')
+
+        # Bring to foreground
+        plt.figure('Odometry')
+
+    def draw(self, x, x_data):
+        # Robot outline
+        th = x[2]
+        angles = th + np.radians([135, 45, -45, -135], dtype=np.float32)
+        corners = (math.sqrt(2) / 2) * np.stack((np.cos(angles, dtype=np.float32), np.sin(angles, dtype=np.float32)), axis=1)
+        corners = np.array([x[0], x[1]], dtype=np.float32) + WIDTH * corners
+        corners = np.append(corners, corners[:1], axis=0)
+        self.robot_line.set_data(*corners.T)
+
+        # Robot heading
+        arrow_dx = 0.25 * LENGTH * math.cos(th)
+        arrow_dy = 0.25 * WIDTH * math.sin(th)
+        self.robot_arrow.set_data(x=x[0], y=x[1], dx=arrow_dx, dy=arrow_dy, head_width=WIDTH / 8)
+
+        # Trajectory (odometry)
+        x_data = np.array(x_data, dtype=np.float32)
+        self.odom_line.set_data(x_data[:, 0], x_data[:, 1])
+
+        # Update plots
+        self.fig_axis.relim()
+        self.fig_axis.autoscale()
+        plt.pause(0.1)
+
+if __name__ == "__main__":
+    # record_state(); exit()
+
+    viz = Visualizer()
+    states = np.load("states.npy")
+    x_replay = deque(maxlen=500)
+
+    i = 0
+    for x in states:
+        print(x)
+        x_replay.append(x)
+        viz.draw(x, x_replay)
