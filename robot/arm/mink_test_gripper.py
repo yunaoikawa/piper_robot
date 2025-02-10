cfc3a27c85e9a1d2683aed2815e9635c1ade230b
Mahi Shafiullah <3000253+notmahi@users.noreply.github.com>
2025-02-10 15:56:26 -0500

    Added arm ruff toml.

--- robot/arm/mink_test_gripper.py
+++ robot/arm/mink_test_gripper.py
@@ -12,80 +12,84 @@ _XML = _HERE / "mujoco_visual" / "scene_gripper.xml"
 
 
 if __name__ == "__main__":
-  model = mujoco.MjModel.from_xml_path(_XML.as_posix())
-  data = mujoco.MjData(model)
+    model = mujoco.MjModel.from_xml_path(_XML.as_posix())
+    data = mujoco.MjData(model)
 
-  ## =================== ##
-  ## Setup IK.
-  ## =================== ##
+    ## =================== ##
+    ## Setup IK.
+    ## =================== ##
 
-  configuration = mink.Configuration(model)
+    configuration = mink.Configuration(model)
 
-  tasks = [
-    end_effector_task := mink.FrameTask(
-      frame_name="attachment_site",
-      frame_type="site",
-      position_cost=100.0,
-      orientation_cost=1.0,
-      lm_damping=1.0,
-    ),
-  ]
-  limits = [
-    mink.ConfigurationLimit(model=model),
-  ]
+    tasks = [
+        end_effector_task := mink.FrameTask(
+            frame_name="attachment_site",
+            frame_type="site",
+            position_cost=100.0,
+            orientation_cost=1.0,
+            lm_damping=1.0,
+        ),
+    ]
+    limits = [
+        mink.ConfigurationLimit(model=model),
+    ]
 
-  max_velocities = {
-    "joint1": np.pi,
-    "joint2": np.pi,
-    "joint3": np.pi,
-    "joint4": np.pi,
-    "joint5": np.pi,
-    "joint6": np.pi,
-  }
+    max_velocities = {
+        "joint1": np.pi,
+        "joint2": np.pi,
+        "joint3": np.pi,
+        "joint4": np.pi,
+        "joint5": np.pi,
+        "joint6": np.pi,
+    }
 
-  velocity_limit = mink.VelocityLimit(model, max_velocities)
-  limits.append(velocity_limit)
+    velocity_limit = mink.VelocityLimit(model, max_velocities)
+    limits.append(velocity_limit)
 
-  ## =================== ##
+    ## =================== ##
 
-  # IK settings.
-  solver = "quadprog"
-  pos_threshold = 1e-3
-  ori_threshold = 1e-3
-  max_iters = 10
+    # IK settings.
+    solver = "quadprog"
+    pos_threshold = 1e-3
+    ori_threshold = 1e-3
+    max_iters = 10
 
-  with mujoco.viewer.launch_passive(model=model, data=data, show_left_ui=False, show_right_ui=False) as viewer:
-    mujoco.mjv_defaultFreeCamera(model, viewer.cam)
+    with mujoco.viewer.launch_passive(
+        model=model, data=data, show_left_ui=False, show_right_ui=False
+    ) as viewer:
+        mujoco.mjv_defaultFreeCamera(model, viewer.cam)
 
-    mujoco.mj_resetDataKeyframe(model, data, model.key("home").id)
-    mujoco.mj_step(model, data)
+        mujoco.mj_resetDataKeyframe(model, data, model.key("home").id)
+        mujoco.mj_step(model, data)
 
-    configuration.update(data.qpos)
-    # posture_task.set_target_from_configuration(configuration)
-    mujoco.mj_forward(model, data)
+        configuration.update(data.qpos)
+        # posture_task.set_target_from_configuration(configuration)
+        mujoco.mj_forward(model, data)
 
-    # Initialize the mocap target at the end-effector site.
-    mink.move_mocap_to_frame(model, data, "target", "attachment_site", "site")
+        # Initialize the mocap target at the end-effector site.
+        mink.move_mocap_to_frame(model, data, "target", "attachment_site", "site")
 
-    rate = RateLimiter(frequency=500.0, warn=False)
-    while viewer.is_running():
-      # Update task target.
-      T_wt = mink.SE3.from_mocap_name(model, data, "target")
-      end_effector_task.set_target(T_wt)
+        rate = RateLimiter(frequency=500.0, warn=False)
+        while viewer.is_running():
+            # Update task target.
+            T_wt = mink.SE3.from_mocap_name(model, data, "target")
+            end_effector_task.set_target(T_wt)
 
-      # Compute velocity and integrate into the next configuration.
-      for i in range(max_iters):
-        vel = mink.solve_ik(configuration, tasks, rate.dt, solver, 1e-3, limits=limits)
-        configuration.integrate_inplace(vel, rate.dt)
-        err = end_effector_task.compute_error(configuration)
-        pos_achieved = np.linalg.norm(err[:3]) <= pos_threshold
-        ori_achieved = np.linalg.norm(err[3:]) <= ori_threshold
-        if pos_achieved and ori_achieved:
-          break
+            # Compute velocity and integrate into the next configuration.
+            for i in range(max_iters):
+                vel = mink.solve_ik(
+                    configuration, tasks, rate.dt, solver, 1e-3, limits=limits
+                )
+                configuration.integrate_inplace(vel, rate.dt)
+                err = end_effector_task.compute_error(configuration)
+                pos_achieved = np.linalg.norm(err[:3]) <= pos_threshold
+                ori_achieved = np.linalg.norm(err[3:]) <= ori_threshold
+                if pos_achieved and ori_achieved:
+                    break
 
-      data.ctrl[:6] = configuration.q[:6]
-      mujoco.mj_step(model, data)
+            data.ctrl[:6] = configuration.q[:6]
+            mujoco.mj_step(model, data)
 
-      # Visualize at fixed FPS.
-      viewer.sync()
-      rate.sleep()
+            # Visualize at fixed FPS.
+            viewer.sync()
+            rate.sleep()
