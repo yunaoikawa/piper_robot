73a41dd725540f3612fb4cabde2906c59f1f4f42
GRAIL NUC 1067 <robotlab-infra@nyu.edu>
2025-02-12 08:09:37 -0500

    rerun viz publish to server

--- robot/rerun/rerun_viz.py
+++ robot/rerun/rerun_viz.py
@@ -1,12 +1,10 @@
 import numpy as np
-
+import cv2
+import blosc as bl
+import zmq
+import pickle
 import rerun as rr
-from dora import Node
-
 from robot.nav.mapping import get_pcd_from_image_and_depth
-
-def get_int_mat(focal, resolution):
-    return np.array([[focal[0], 0, resolution[0]], [0, focal[1], resolution[1]], [0, 0, 1]], dtype=np.float32)
 
 
 def log_image(event):
@@ -36,25 +34,7 @@ def log_pose(all_poses, event):
         rr.log("world/camera", rr.Transform3D(translation=translation, rotation=rr.Quaternion(xyzw=quaternion)))
     return all_poses
 
-def log_map(curr_map, all_poses, image_event, depth_event, curr_confidence_event, pose_event):
-    image_time = float(image_event["metadata"]["timestamp"])
-    depth_time = float(depth_event["metadata"]["timestamp"])
-    pose_time = float(pose_event["metadata"]["timestamp"])
-    max_diff = max(abs(image_time - depth_time), abs(image_time - pose_time), abs(depth_time - pose_time))
-    if max_diff > 0.04:
-        print(f"Error: Event timestamps not synchronized. Max difference: {max_diff:.3f}s")
-        return
-
-    print("Logging map")
-
-    image = image_event["value"].to_numpy().astype(np.uint8).reshape((image_event["metadata"]["height"], image_event["metadata"]["width"], 3))
-    depth = depth_event["value"].to_numpy().astype(np.float32).reshape((depth_event["metadata"]["height"], depth_event["metadata"]["width"]))
-    confidence = curr_confidence_event["value"].to_numpy().astype(np.uint8).reshape((depth_event["metadata"]["height"], depth_event["metadata"]["width"]))
-    pose = pose_event["value"].to_numpy().astype(np.float32)
-
-    focal = depth_event["metadata"]["focal"]
-    resolution = depth_event["metadata"]["resolution"]
-
+def log_map(curr_map, all_poses, image, depth, confidence, pose, focal, resolution):
     all_poses.append(pose)
     rr.log("world/pose", rr.Points3D(positions=[pose[4:7] for pose in all_poses], radii=[0.025 for _ in all_poses]))
 
@@ -71,43 +51,38 @@ def log_map(curr_map, all_poses, image_event, depth_event, curr_confidence_event
     rr.log("world/map", rr.Points3D(positions=np.asarray(curr_map.points), colors=np.asarray(curr_map.colors)))
     return curr_map, all_poses
 
-
 def main():
-    node = Node("rerun")
+    robot_ip = ""
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.setsockopt_string(zmq.SUBSCRIBE, b"map_info")
+
+    socket.connect(f"tcp://{robot_ip}:5555")
 
     rr.init("rerun_visualizer")
     # rr.serve_web(open_browser=False, server_memory_limit="1GB")
     rr.save("rerun_test.rrd")
     rr.log("world/axis", rr.Transform3D(translation=[0, 0, 0], rotation=rr.Quaternion(xyzw=[1, 0, 0, 0]), axis_length=0.5), static=True)
 
-    curr_image_event = None
-    curr_depth_event = None
-    curr_confidence_event = None
-    curr_pose_event = None
-
     curr_map = None
     all_poses = []
 
-    for event in node:
-        if event["type"] == "INPUT":
-            if event["id"] == "iphone/image":
-                curr_image_event = event
-            elif event["id"] == "iphone/depth":
-                curr_depth_event = event
-            elif event["id"] == "iphone/pose":
-                curr_pose_event = event
-            elif event["id"] == "iphone/confidence":
-                curr_confidence_event = event
-            elif event["id"] == "tick":
-                if curr_image_event is not None: log_image(curr_image_event)
-                if curr_depth_event is not None: log_depth(curr_depth_event)
-                if curr_confidence_event is not None: pass
-                # if curr_pose_event is not None: all_poses = log_pose(all_poses, curr_pose_event)
+    try:
+        while True:
+            message = socket.recv()
+            message = message.lstrip(b"map_info")
+            data = pickle.loads(message)
 
-                if curr_image_event is not None and curr_depth_event is not None and curr_confidence_event is not None and curr_pose_event is not None:
-                    curr_map, all_poses = log_map(curr_map, all_poses, curr_image_event, curr_depth_event, curr_confidence_event, curr_pose_event)
+            encoded_image = np.frombuffer(data["image"], np.uint8)
+            image = cv2.imdecode(encoded_image, 1)
+            depth = np.array(bl.unpack_array(data["depth"]), dtype=np.float32)
+            confidence = np.array(bl.unpack_array(data["confidence"]), dtype=np.uint8)
+            pose = np.array(data["pose"], dtype=np.float32)
+            focal = data["focal"]
+            resolution = data["resolution"]
 
-    rr.disconnect()
-if __name__ == "__main__":
-    main()
+            curr_map, all_poses = log_map(curr_map, all_poses, image, depth, confidence, pose, focal, resolution)
+    finally:
+        rr.disconnect()
+
 
---
+++ robot/rerun/viz_publisher.py
@@ -0,0 +1,67 @@
+import numpy as np
+import zmq
+import cv2
+import blosc as bl
+import pickle
+
+from dora import Node
+
+def pub_map_info(socket: zmq.Socket, image_event, depth_event, curr_confidence_event, pose_event):
+    image_time = float(image_event["metadata"]["timestamp"])
+    depth_time = float(depth_event["metadata"]["timestamp"])
+    pose_time = float(pose_event["metadata"]["timestamp"])
+    max_diff = max(abs(image_time - depth_time), abs(image_time - pose_time), abs(depth_time - pose_time))
+    if max_diff > 0.04:
+        print(f"Error: Event timestamps not synchronized. Max difference: {max_diff:.3f}s")
+        return
+
+    image = image_event["value"].to_numpy().astype(np.uint8).reshape((image_event["metadata"]["height"], image_event["metadata"]["width"], 3))
+    depth = depth_event["value"].to_numpy().astype(np.float32).reshape((depth_event["metadata"]["height"], depth_event["metadata"]["width"]))
+    confidence = curr_confidence_event["value"].to_numpy().astype(np.uint8).reshape((depth_event["metadata"]["height"], depth_event["metadata"]["width"]))
+    pose = pose_event["value"].to_numpy().astype(np.float32)
+
+    _, buffer = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+    compressed_depth = bl.pack_array(
+        depth, cname="zstd", clevel=1, shuffle=bl.NOSHUFFLE
+    )
+    compressed_confidence = bl.pack_array(
+        confidence, cname="zstd", clevel=1, shuffle=bl.NOSHUFFLE
+    )
+    data = {"image": buffer, "depth": compressed_depth, "confidence": compressed_confidence, "pose": pose}
+    socket.send(b"map_info" + pickle.dumps(data, protocol=-1))
+
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://*:5555")
+
+    node = Node("rerun")
+
+    curr_image_event = None
+    curr_depth_event = None
+    curr_confidence_event = None
+    curr_pose_event = None
+
+    # curr_map = None
+    # all_poses = []
+
+    for event in node:
+        if event["type"] == "INPUT":
+            if event["id"] == "iphone/image":
+                curr_image_event = event
+            elif event["id"] == "iphone/depth":
+                curr_depth_event = event
+            elif event["id"] == "iphone/pose":
+                curr_pose_event = event
+            elif event["id"] == "iphone/confidence":
+                curr_confidence_event = event
+            elif event["id"] == "tick":
+
+                if curr_image_event is not None and curr_depth_event is not None and curr_confidence_event is not None and curr_pose_event is not None:
+                    pub_map_info(socket, curr_image_event, curr_depth_event, curr_confidence_event, curr_pose_event)
+
+
+if __name__ == "__main__":
+    main()
+
