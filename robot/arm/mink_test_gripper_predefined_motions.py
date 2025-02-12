b1a49f732b08ab283eded859ec54c927f8e79a0f
Mahi Shafiullah <3000253+notmahi@users.noreply.github.com>
2025-02-12 10:48:16 -0500

    Added a way to test the robot doing pre-defined motions.

--- robot/arm/mink_test_gripper_predefined_motions.py
+++ robot/arm/mink_test_gripper_predefined_motions.py
@@ -1,17 +1,151 @@
+import argparse
+from dataclasses import dataclass
 from pathlib import Path
+
+import mink
+import numpy as np
+import pandas as pd
+from dm_control.viewer import user_input
+from loop_rate_limiters import RateLimiter
+from scipy.spatial.transform import Rotation as R
 
 import mujoco
 import mujoco.viewer
-import numpy as np
-from loop_rate_limiters import RateLimiter
-
-import mink
 
 _HERE = Path(__file__).parent
 _XML = _HERE / "mujoco_visual" / "scene_gripper.xml"
+DATASET_PATH = Path("~/projects/local-code/robot-utility-model-data/train").expanduser()
 
+TASKS = {
+    "door": "Door_Opening",
+    "drawer": "Drawer_Opening",
+    "tissue": "Tissue_Pick_Up",
+    "bag": "Bag_Pick_Up",
+    "bottle": "Reorientation",
+}
+
+
+@dataclass
+class KeyCallback:
+    fix_base: bool = False
+    pause: bool = False
+
+    def __call__(self, key: int) -> None:
+        if key == user_input.KEY_ENTER:
+            self.fix_base = not self.fix_base
+        elif key == user_input.KEY_SPACE:
+            self.pause = not self.pause
+
+
+def add_3color_axes_to_viewer(pos, rotation, viewer, length=0.25):
+    scene = viewer.user_scn
+    if scene.ngeom >= scene.maxgeom:
+        return
+    scene.ngeom += 3  # increment ngeom
+    # X-axis (Red)
+    # rotation = np.eye(3)
+    R_local_to_world = np.column_stack([rotation[:, 1], rotation[:, 2], rotation[:, 0]])
+    mujoco.mjv_initGeom(
+        geom=scene.geoms[scene.ngeom - 3],
+        type=mujoco.mjtGeom.mjGEOM_ARROW,
+        size=np.array([0.01, 0.01, length]),
+        rgba=np.array([1, 0, 0, 0.5]),
+        pos=pos,
+        mat=R_local_to_world.flatten(),  # Rotate to align with X
+    )
+
+    # Y-axis (Green)
+    R_local_to_world_y = np.column_stack([
+        -rotation[:, 0],
+        -rotation[:, 2],
+        rotation[:, 1],
+    ])
+    mujoco.mjv_initGeom(
+        geom=scene.geoms[scene.ngeom - 2],
+        type=mujoco.mjtGeom.mjGEOM_ARROW,
+        size=np.array([0.01, 0.01, length]),
+        rgba=np.array([0, 1, 0, 0.5]),
+        pos=pos,
+        mat=R_local_to_world_y.flatten(),  # Rotate 90° to align with Y
+    )
+
+    # Z-axis (Blue)
+    mujoco.mjv_initGeom(
+        geom=scene.geoms[scene.ngeom - 1],
+        type=mujoco.mjtGeom.mjGEOM_ARROW,
+        size=np.array([0.01, 0.01, length]),
+        rgba=np.array([0, 0, 1, 0.5]),
+        pos=pos,
+        mat=rotation.flatten(),  # Identity matrix for alignment
+    )
+
+
+# This transform should take the data from the dataset and transform it to the
+# coordinate frame of the robot end-effector.
+# The data in our datasets follows the OpenCV convention:
+# x -> right, y -> down, z -> forward
+# The robot end-effector has the following convention:
+# x -> down, y -> left, z -> forward.
+DATA_TRANSFORM = np.array([
+    [0, 1, 0],
+    [-1, 0, 0],
+    [0, 0, 1],
+])
+
+
+def get_random_target_trajectory(task, seed=424242):
+    np.random.seed(seed)
+    target_task = TASKS[task]
+    parquet_path_glob = (DATASET_PATH / target_task).glob("**/*.parquet")
+    parquet_path_list = list(parquet_path_glob)
+    parquet_path = np.random.choice(parquet_path_list)
+    trajectory_df = pd.read_parquet(parquet_path)
+    gripper_values = trajectory_df["gripper"].values
+    xyz = trajectory_df[["x", "y", "z"]].values
+    quat = trajectory_df[["qx", "qy", "qz", "qw"]].values
+    rotation_matrices = R.from_quat(quat, scalar_first=False).as_matrix()
+
+    transformed_xyz = np.dot(xyz, DATA_TRANSFORM.T)
+    transformed_rot = np.matmul(
+        DATA_TRANSFORM[None], np.matmul(rotation_matrices, DATA_TRANSFORM.T[None])
+    )
+
+    SE3_matrices = np.zeros((len(xyz), 4, 4))
+    SE3_matrices[:, :3, :3] = transformed_rot
+    SE3_matrices[:, :3, 3] = transformed_xyz
+    SE3_matrices[:, 3, 3] = 1
+    return SE3_matrices, gripper_values
+
+
+parser = argparse.ArgumentParser(
+    prog="Mink IK test",
+)
+parser.add_argument(
+    "--task",
+    type=str,
+    choices=TASKS.keys(),
+    help="Task to perform open-loop.",
+    required=True,
+    default="door",
+)
+parser.add_argument(
+    "--seed",
+    type=int,
+    help="Seed for random trajectory selection.",
+    default=424242,
+)
+parser.add_argument(
+    "--fps",
+    type=float,
+    help="Frames per second in the input data",
+    default=30.0,
+)
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    random_traj = get_random_target_trajectory(task=args.task, seed=args.seed)
+    fps = args.fps
+
     model = mujoco.MjModel.from_xml_path(_XML.as_posix())
     data = mujoco.MjData(model)
 
@@ -53,14 +187,20 @@ if __name__ == "__main__":
     pos_threshold = 1e-3
     ori_threshold = 1e-3
     max_iters = 10
+    key_callback = KeyCallback()
 
     with mujoco.viewer.launch_passive(
-        model=model, data=data, show_left_ui=True, show_right_ui=False
+        model=model,
+        data=data,
+        show_left_ui=False,
+        show_right_ui=False,
+        key_callback=key_callback,
     ) as viewer:
+        add_3color_axes_to_viewer(np.ones(3), np.eye(3), viewer)
         viewer.opt.frame = mujoco.mjtFrame.mjFRAME_SITE
         mujoco.mjv_defaultFreeCamera(model, viewer.cam)
 
-        mujoco.mj_resetDataKeyframe(model, data, model.key("home").id)
+        mujoco.mj_resetDataKeyframe(model, data, model.key("tasks").id)
         mujoco.mj_step(model, data)
 
         configuration.update(data.qpos)
@@ -71,10 +211,34 @@ if __name__ == "__main__":
         mink.move_mocap_to_frame(model, data, "target", "attachment_site", "site")
 
         rate = RateLimiter(frequency=200.0, warn=False)
+        task_trajectory, gripper_values = random_traj
+        current_target_idx = 0
+        current_time = 0
+        init_T_wt = mink.SE3.from_mocap_name(model, data, "target")
+        done = False
+        target_T_wt = init_T_wt.copy()
+        draw_every = 10
+
         while viewer.is_running():
+            current_time += rate.dt
+            if (current_time * fps) >= current_target_idx and not done:
+                current_target_idx += 1
+                if current_target_idx >= len(task_trajectory):
+                    done = True
             # Update task target.
-            T_wt = mink.SE3.from_mocap_name(model, data, "target")
-            end_effector_task.set_target(T_wt)
+            if not done:
+                target_SE3 = task_trajectory[current_target_idx]
+                target_T_wt = init_T_wt.copy().multiply(
+                    mink.SE3.from_matrix(target_SE3)
+                )
+                if current_target_idx % draw_every == 0:
+                    add_3color_axes_to_viewer(
+                        target_T_wt.copy().translation(),
+                        target_T_wt.copy().rotation().as_matrix(),
+                        viewer,
+                    )
+            # Update task target.
+            end_effector_task.set_target(target_T_wt)
 
             # Compute velocity and integrate into the next configuration.
             for i in range(max_iters):
@@ -88,7 +252,7 @@ if __name__ == "__main__":
                 if pos_achieved and ori_achieved:
                     break
 
-            data.ctrl[:6] = configuration.q[:6]
+            data.ctrl = configuration.q[:7]
             mujoco.mj_step(model, data)
 
             # Visualize at fixed FPS.
