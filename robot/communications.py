8242e47e55b6b64ff146ec94c85c54bf3d21b6e3
eneserciyes <m.eneserciyes@gmail.com>
2025-02-14 13:50:43 -0500

    zmq stack back

---
+++ robot/communications.py
@@ -0,0 +1,45 @@
+import zmq
+from typing import Any, List, Callable, Tuple
+
+# Networking Constants
+ROBOT_IP = "100.96.33.32"  # tailscale ip
+COMMAND_PORT = 5555
+ROBOT_STATE_PORT = 5556
+BASE_CAMERA_PORT = 9000
+
+
+class Publisher:
+  def __init__(self, ctx: zmq.Context, port: int, serializer: Callable[[Any], Tuple[bytes, bytes]], host: str = "*"):
+    self.socket: zmq.Socket = ctx.socket(zmq.PUB)
+    self.socket.bind(f"tcp://{host}:{port}")
+    self.serializer = serializer
+
+  def publish(self, topic: str, data: Any, copy: bool = True):
+    metadata, data = self.serializer(data)
+    self.socket.send_string(topic, zmq.SNDMORE)
+    self.socket.send(metadata, zmq.SNDMORE)
+    self.socket.send(data, copy=copy)
+
+  def stop(self):
+    self.socket.close()
+
+
+class Subscriber:
+  def __init__(self, ctx: zmq.Context, port: int, topics: List[str], deserializer: callable, host: str = "localhost", conflate: bool = True):
+    self.socket: zmq.Socket = ctx.socket(zmq.SUB)
+    self.socket.connect(f"tcp://{host}:{port}")
+    for topic in topics:
+      self.socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+    if conflate:
+      self.socket.setsockopt(zmq.CONFLATE, 1)
+    self.deserializer = deserializer
+
+  def receive(self) -> tuple[str, Any]:
+    topic, metadata, data = self.socket.recv_multipart()
+    return topic.decode("utf-8"), self.deserializer(metadata.decode("utf-8"), data)
+
+  def register_poller(self, poller: zmq.Poller):
+    poller.register(self.socket, zmq.POLLIN)
+
+  def stop(self):
+    self.socket.close()
