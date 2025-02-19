281c509bed3d5e3dac994378cfe517718500157a
Mahi Shafiullah <3000253+notmahi@users.noreply.github.com>
2025-02-18 23:12:55 -0500

    Refactored code into components.

--- robot/arm/mink_control_bimanual.py
+++ robot/arm/mink_control_bimanual.py
@@ -1,44 +1,14 @@
 import argparse
-import signal
-import time
 from pathlib import Path
 
 import mink
 import numpy as np
 from loop_rate_limiters import RateLimiter
-from piper_sdk import C_PiperInterface
 
-import constants
 import mujoco
 import mujoco.viewer
-
-
-class SignalBlocker:
-    def __enter__(self):
-        """Block SIGINT (CTRL+C) and SIGTERM, but store if any were received."""
-        self.received_signal = None  # Store interrupted signal
-
-        def handler(signum, frame):
-            """Custom handler to store received signal instead of acting immediately."""
-            print(
-                f"\nSignal {signum} received, delaying execution until after the block."
-            )
-            self.received_signal = (signum, frame)
-
-        # Save original handlers
-        self.original_sigint = signal.signal(signal.SIGINT, handler)
-        self.original_sigterm = signal.signal(signal.SIGTERM, handler)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        """Restore original signal handlers and re-raise signal if one was captured."""
-        signal.signal(signal.SIGINT, self.original_sigint)
-        signal.signal(signal.SIGTERM, self.original_sigterm)
-
-        if self.received_signal:
-            signum, frame = self.received_signal
-            print(f"\nReplaying signal {signum} now...")
-            self.original_sigint(signum, frame)  # Re-execute the original handler
-
+import piper_utils
+import utils
 
 _HERE = Path(__file__).parent
 _XML = _HERE / "mujoco_visual" / "shoulder_mounted_pipers.xml"
@@ -49,58 +19,17 @@ WORLD_TO_SITE = np.array([
 ])
 
 
-def enable_fun(piper: C_PiperInterface):
-    enable_flag = False
-    timeout = 5
-    start_time = time.time()
-    elapsed_time_flag = False
-    while not (enable_flag):
-        elapsed_time = time.time() - start_time
-        print("--------------------")
-        enable_flag = (
-            piper.GetArmLowSpdInfoMsgs().motor_1.foc_status.driver_enable_status
-            and piper.GetArmLowSpdInfoMsgs().motor_2.foc_status.driver_enable_status
-            and piper.GetArmLowSpdInfoMsgs().motor_3.foc_status.driver_enable_status
-            and piper.GetArmLowSpdInfoMsgs().motor_4.foc_status.driver_enable_status
-            and piper.GetArmLowSpdInfoMsgs().motor_5.foc_status.driver_enable_status
-            and piper.GetArmLowSpdInfoMsgs().motor_6.foc_status.driver_enable_status
-        )
-        print("Enable status:", enable_flag)
-        piper.EnableArm(7)
-        piper.GripperCtrl(0, 1000, 0x01, 0)
-        print("--------------------")
-        if elapsed_time > timeout:
-            print("Timeout....")
-            elapsed_time_flag = True
-            enable_flag = True
-            break
-        time.sleep(1)
-        pass
-    if elapsed_time_flag:
-        print("The program automatically enables timeout, exit the program")
-        exit(0)
-
-
-def scale_and_clip_control(q: np.ndarray) -> np.ndarray:
-    min_val = np.array(constants.ARM_JOINT_LIMITS_MIN)
-    max_val = np.array(constants.ARM_JOINT_LIMITS_MAX)
-    N = len(constants.ARM_JOINT_LIMITS_MAX)
-
-    clipped_q = np.zeros(N + 1)
-    clipped_q[:N] = np.round(constants.ARM_JOINT_SCALING_CONSTANT * q[:N])
-    clipped_q[:N] = np.clip(clipped_q[:N], a_min=min_val, a_max=max_val)
-
-    clipped_q[N] = np.round(constants.WRIST_SCALING_CONSTANT * q[N])
-
-    return clipped_q.astype(int)
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--real", action="store_true")
+parser.add_argument("--headless", action="store_true")
+parser.add_argument("--task", type=str, default=None, choices=utils.TASKS.keys())
+parser.add_argument("--use_left", action="store_true")
+parser.add_argument("--demo_fps", type=int, default=30)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     use_real = args.real
+    fps = args.demo_fps
     model = mujoco.MjModel.from_xml_path(_XML.as_posix())
     data = mujoco.MjData(model)
 
@@ -168,7 +97,7 @@ if __name__ == "__main__":
     rate = RateLimiter(frequency=200.0, warn=False)
     current_target_idx = 0
     current_time = 0
-    mujoco.mj_resetDataKeyframe(model, data, model.key("home").id)
+    mujoco.mj_resetDataKeyframe(model, data, model.key("optim").id)
     mujoco.mj_step(model, data)
 
     configuration.update(data.qpos)
@@ -179,8 +108,7 @@ if __name__ == "__main__":
         model, data, "right_target", "right_attachment_site", "site"
     )
     mink.move_mocap_to_frame(model, data, "left_target", "left_attachment_site", "site")
-    # TODO fix this
-    use_left = False
+    use_left = args.use_left
     # Make the IK solver arm-agnostic. We should be able to do the tasks with either arm
     target_name = "left_target" if use_left else "right_target"
     other_target_name = "right_target" if use_left else "left_target"
@@ -188,22 +116,38 @@ if __name__ == "__main__":
     other_eef_task = right_end_effector_task if use_left else left_end_effector_task
     left_gripper_idx = 13
     right_gripper_idx = 6
+
+    # If task is given, use task target.
+    if args.task:
+        trajectory, gripper_trajectory = utils.get_random_target_trajectory(args.task)
+    else:
+        trajectory, gripper_trajectory = None, None
+
     # Now set the static target for the other arm.
     other_arm_init_T_wt_with_rot = mink.SE3.from_mocap_name(
         model, data, other_target_name
     )
+    init_T_wt_with_rot = mink.SE3.from_mocap_name(model, data, target_name)
+
     other_arm_init_T_wt = mink.SE3.from_rotation_and_translation(
         translation=other_arm_init_T_wt_with_rot.translation(),
         rotation=mink.SO3.from_matrix(WORLD_TO_SITE),
     )
     other_eef_task.set_target(other_arm_init_T_wt)
+    init_T_wt = mink.SE3.from_rotation_and_translation(
+        translation=init_T_wt_with_rot.translation(),
+        rotation=mink.SO3.from_matrix(WORLD_TO_SITE),
+    )
 
-    def run_simulation_step():
+    def run_simulation_step(target_SE3=None, other_target_SE3=None):
+        if target_SE3 is None:
+            target_SE3 = mink.SE3.from_mocap_name(model, data, target_name)
+
+        if other_target_SE3 is None:
+            other_target_SE3 = mink.SE3.from_mocap_name(model, data, other_target_name)
+
         # Update task target.
-        target_SE3 = mink.SE3.from_mocap_name(model, data, target_name)
         end_effector_task.set_target(target_SE3)
-
-        other_target_SE3 = mink.SE3.from_mocap_name(model, data, other_target_name)
         other_eef_task.set_target(other_target_SE3)
 
         # Solve IK in a few iterations
@@ -225,58 +169,48 @@ if __name__ == "__main__":
 
         return True, configuration.q
 
-    # Get initial target pose for reference:
-    init_T_wt_with_rot = mink.SE3.from_mocap_name(model, data, target_name)
-    init_T_wt = mink.SE3.from_rotation_and_translation(
-        translation=init_T_wt_with_rot.translation(),
-        rotation=mink.SO3.from_matrix(WORLD_TO_SITE),
-    )
-
     if use_real:
         # Start the piper controllers.
-        piper_left = C_PiperInterface("can_left")
-        piper_left.ConnectPort()
-        piper_left.EnableArm(7)
-        enable_fun(piper=piper_left)
-        piper_right = C_PiperInterface("can_right", can_auto_init=False)
-        piper_right.ConnectPort()
-        piper_right.EnableArm(7)
-        enable_fun(piper=piper_right)
+        piper_left, piper_right = piper_utils.setup_arms(
+            left_name="can_left", right_name="can_right"
+        )
 
-    with mujoco.viewer.launch_passive(
-        model=model,
-        data=data,
-        show_left_ui=False,
-        show_right_ui=False,
-    ) as viewer:
-        viewer.opt.frame = mujoco.mjtFrame.mjFRAME_SITE
-        mujoco.mjv_defaultFreeCamera(model, viewer.cam)
+    current_t = 0
 
-        frame_count = 0
+    if not args.headless:
+        with mujoco.viewer.launch_passive(
+            model=model,
+            data=data,
+            show_left_ui=False,
+            show_right_ui=False,
+        ) as viewer:
+            viewer.opt.frame = mujoco.mjtFrame.mjFRAME_SITE
+            mujoco.mjv_defaultFreeCamera(model, viewer.cam)
 
-        while viewer.is_running():
-            # Step the simulation
-            is_ok, q = run_simulation_step()
-            if not is_ok:
-                break
+            frame_count = 0
 
-            if use_real:
-                # with SignalBlocker():
-                right_q, left_q = q[:6].copy(), q[8:14].copy()
+            while viewer.is_running():
+                # Step the simulation
+                current_t += rate.dt
+                current_target_SE3 = None
+                if trajectory is not None:
+                    if len(trajectory) > int(current_t * fps):
+                        current_target = trajectory[int(current_t * fps)]
+                        current_target_SE3 = init_T_wt.multiply(
+                            mink.SE3.from_matrix(current_target)
+                        )
+                    else:
+                        break
 
-                right_joint_values = scale_and_clip_control(right_q).tolist()
-                piper_right.MotionCtrl_2(0x01, 0x01, 50, 0x00)
-                piper_right.JointCtrl(*right_joint_values[:6])
-                # piper.GripperCtrl(abs(joint_6), 1000, 0x01, 0)
-                piper_right.MotionCtrl_2(0x01, 0x01, 50, 0x00)
+                is_ok, q = run_simulation_step(current_target_SE3)
+                if not is_ok:
+                    break
 
-                left_joint_values = scale_and_clip_control(left_q).tolist()
-                piper_left.MotionCtrl_2(0x01, 0x01, 50, 0x00)
-                piper_left.JointCtrl(*left_joint_values[:6])
-                # piper.GripperCtrl(abs(joint_6), 1000, 0x01, 0)
-                piper_left.MotionCtrl_2(0x01, 0x01, 50, 0x00)
+                if use_real:
+                    # with SignalBlocker():
+                    piper_utils.send_to_arms(piper_left, piper_right, q)
 
-            # Render
-            viewer.sync()
-            rate.sleep()
-            frame_count += 1
+                # Render
+                viewer.sync()
+                rate.sleep()
+                frame_count += 1
