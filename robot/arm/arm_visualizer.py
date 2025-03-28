74ea88192a15a50cee6190f6376d6e57d8f6af5c
eneserciyes <m.eneserciyes@gmail.com>
2025-03-28 16:07:44 -0400

    fix gripper joints, remove reduced_robot

--- robot/arm/arm_visualizer.py
+++ robot/arm/arm_visualizer.py
@@ -23,30 +23,26 @@ def print_frame_info(robot_model):
 # Add this after creating the robot:
 
 @click.command()
-@click.argument("urdf_name", type=str, default="bimanual")
+@click.argument("urdf_name", type=str, default="bimanual_humanoid")
 def main(urdf_name):
     urdf_file = os.path.join(os.path.dirname(__file__), "models", urdf_name+".urdf")
     mesh_dir = os.path.join(os.path.dirname(__file__), "models", "meshes")
 
     # Build the robot with the package directories
     robot = pin.RobotWrapper.BuildFromURDF(urdf_file, package_dirs=[mesh_dir])
-    reduced_robot = robot.buildReducedRobot(
-        list_of_joints_to_lock=["left_joint7", "left_joint8", "right_joint7", "right_joint8"],
-        reference_configuration=np.array([0] * robot.model.nq),
-    )
 
-    lift_middle_joint_id = reduced_robot.model.getJointId("lift_middle_joint")-1
-    lift_top_joint_id = reduced_robot.model.getJointId("lift_top_joint")-1
-    left_joint2_id = reduced_robot.model.getJointId("left_joint2")-1
-    left_joint3_id = reduced_robot.model.getJointId("left_joint3")-1
-    right_joint2_id = reduced_robot.model.getJointId("right_joint2")-1
-    right_joint3_id = reduced_robot.model.getJointId("right_joint3")-1
+    lift_middle_joint_id = robot.model.getJointId("lift_middle_joint")-1
+    lift_top_joint_id = robot.model.getJointId("lift_top_joint")-1
+    left_joint2_id = robot.model.getJointId("left_joint2")-1
+    left_joint3_id = robot.model.getJointId("left_joint3")-1
+    right_joint2_id = robot.model.getJointId("right_joint2")-1
+    right_joint3_id = robot.model.getJointId("right_joint3")-1
 
-    viz = MeshcatVisualizer(reduced_robot.model, reduced_robot.collision_model, reduced_robot.visual_model)
+    viz = MeshcatVisualizer(robot.model, robot.collision_model, robot.visual_model)
     viz.initViewer(open=False)
     viz.loadViewerModel("pinocchio")
 
-    neutral_config = pin.neutral(reduced_robot.model)
+    neutral_config = pin.neutral(robot.model)
     neutral_config[lift_middle_joint_id] = 0.1
     neutral_config[lift_top_joint_id] = 0.1
     neutral_config[left_joint2_id] = 3.14
@@ -55,12 +51,30 @@ def main(urdf_name):
     neutral_config[right_joint3_id] = -2.967
     viz.display(neutral_config)
 
-    # Display frame for torso_link with a visible axis length and width
-    # torso_frame_id = reduced_robot.model.getFrameId("torso_joint")-1
-    # print(f"Torso frame ID: {torso_frame_id}")
-    # viz.displayFrames(True, frame_ids=[torso_frame_id], axis_length=0.2, axis_width=5)
+    while True:
+        time.sleep(1)
+
+def main_piper():
+    urdf_file = os.path.join(os.path.dirname(__file__), "models", "piper-right.urdf")
+    mesh_dir = os.path.join(os.path.dirname(__file__), "models", "meshes")
+
+    # Build the robot with the package directories
+    robot = pin.RobotWrapper.BuildFromURDF(urdf_file, package_dirs=[mesh_dir])
+    reduced_robot = robot.buildReducedRobot(
+        list_of_joints_to_lock=["joint7", "joint8"],
+        reference_configuration=np.array([0] * robot.model.nq),
+    )
+    print_frame_info(reduced_robot.model)
+
+    viz = MeshcatVisualizer(reduced_robot.model, reduced_robot.collision_model, reduced_robot.visual_model)
+    viz.initViewer(open=False)
+    viz.loadViewerModel("pinocchio")
+
+    neutral_config = pin.neutral(reduced_robot.model)
+    viz.display(neutral_config)
 
     while True:
         time.sleep(1)
+
 if __name__ == "__main__":
     main()
