966778318d8342bf374880920fa8fbef376fc52f
eneserciyes <m.eneserciyes@gmail.com>
2025-03-29 00:14:48 -0400

    move ee_left and fix arm velocities

--- robot/arm/arm_visualizer.py
+++ robot/arm/arm_visualizer.py
@@ -1,10 +1,18 @@
 import os
-import numpy as np
-import time
 import click
 
+import meshcat_shapes
+import numpy as np
 import pinocchio as pin
-from pinocchio.visualize import MeshcatVisualizer
+import qpsolvers
+from loop_rate_limiters import RateLimiter
+
+import pink
+from pink import solve_ik
+from pink.tasks import FrameTask
+from pink.utils import custom_configuration_vector
+from pink.visualization import start_meshcat_visualizer
+
 
 def print_joint_info(robot_model):
     print("\nJoint Information:")
@@ -13,6 +21,7 @@ def print_joint_info(robot_model):
         joint_name = robot_model.names[joint_id]
         print(f"Joint ID: {joint_id}, Name: {joint_name}")
 
+
 def print_frame_info(robot_model):
     print("\nFrame Information:")
     print("------------------")
@@ -20,59 +29,89 @@ def print_frame_info(robot_model):
         frame_name = robot_model.frames[frame_id].name
         print(f"Frame ID: {frame_id}, Name: {frame_name}")
 
+
 # Add this after creating the robot:
+
 
 @click.command()
 @click.argument("urdf_name", type=str, default="bimanual_humanoid")
 def main(urdf_name):
-    urdf_file = os.path.join(os.path.dirname(__file__), "models", urdf_name+".urdf")
+    urdf_file = os.path.join(os.path.dirname(__file__), "models", urdf_name + ".urdf")
     mesh_dir = os.path.join(os.path.dirname(__file__), "models", "meshes")
 
     # Build the robot with the package directories
-    robot = pin.RobotWrapper.BuildFromURDF(urdf_file, package_dirs=[mesh_dir])
+    robot = pin.RobotWrapper.BuildFromURDF(urdf_file, package_dirs=[mesh_dir], root_joint=None)
 
-    lift_joint_id = robot.model.getJointId("lift_joint")-1
-    left_joint2_id = robot.model.getJointId("left_joint2")-1
-    left_joint3_id = robot.model.getJointId("left_joint3")-1
-    right_joint2_id = robot.model.getJointId("right_joint2")-1
-    right_joint3_id = robot.model.getJointId("right_joint3")-1
+    print(f"URDF description successfully loaded in {robot}")
 
-    viz = MeshcatVisualizer(robot.model, robot.collision_model, robot.visual_model)
-    viz.initViewer(open=False)
-    viz.loadViewerModel("pinocchio")
+    viz = start_meshcat_visualizer(robot)
 
-    neutral_config = pin.neutral(robot.model)
-    neutral_config[lift_joint_id] = 0.15
-    neutral_config[left_joint2_id] = 3.14
-    neutral_config[left_joint3_id] = -2.967
-    neutral_config[right_joint2_id] = 3.14
-    neutral_config[right_joint3_id] = -2.967
-    viz.display(neutral_config)
+    meshcat_shapes.frame(viz.viewer["lift"], opacity=1.0)
+    meshcat_shapes.frame(viz.viewer["lift_target"], opacity=0.5)
+    meshcat_shapes.frame(viz.viewer["ee_left"], opacity=1.0)
+    meshcat_shapes.frame(viz.viewer["ee_left_target"], opacity=0.5)
 
-    while True:
-        time.sleep(1)
-
-def main_piper():
-    urdf_file = os.path.join(os.path.dirname(__file__), "models", "piper-right.urdf")
-    mesh_dir = os.path.join(os.path.dirname(__file__), "models", "meshes")
-
-    # Build the robot with the package directories
-    robot = pin.RobotWrapper.BuildFromURDF(urdf_file, package_dirs=[mesh_dir])
-    reduced_robot = robot.buildReducedRobot(
-        list_of_joints_to_lock=["joint7", "joint8"],
-        reference_configuration=np.array([0] * robot.model.nq),
+    lift_task = FrameTask(
+        "lift_top",
+        position_cost=0.0,
+        orientation_cost=0.0,
     )
-    print_frame_info(reduced_robot.model)
 
-    viz = MeshcatVisualizer(reduced_robot.model, reduced_robot.collision_model, reduced_robot.visual_model)
-    viz.initViewer(open=False)
-    viz.loadViewerModel("pinocchio")
+    left_ee_task = FrameTask(
+        "ee_left",
+        position_cost=1.0,
+        orientation_cost=1.0,
+    )
 
-    neutral_config = pin.neutral(reduced_robot.model)
-    viz.display(neutral_config)
+    tasks = [lift_task, left_ee_task]
+
+    q_ref = custom_configuration_vector(
+        robot,
+        lift_joint=0.15,
+        left_joint2=3.14,
+        left_joint3=-2.967,
+        right_joint2=3.14,
+        right_joint3=-2.967,
+    )
+
+    configuration = pink.Configuration(robot.model, robot.data, q_ref)
+
+    for task in tasks:
+        task.set_target_from_configuration(configuration)
+    viz.display(configuration.q)
+
+    input("Press Enter to continue...")
+
+    # Select the solver
+    solver = qpsolvers.available_solvers[0]
+    if "quadprog" in qpsolvers.available_solvers:
+        solver = "quadprog"
+
+    rate = RateLimiter(frequency=200, warn=False)
+    dt = rate.period
+    t = 0.0  # [s]
 
     while True:
-        time.sleep(1)
+        # lift_target = lift_task.transform_target_to_world
+        # lift_target.translation[2] = 0.3 + 0.15 * np.sin(2 * np.pi * 0.5 * t)  # Oscillates between 0 and 0.3 at 0.5 Hz
+
+        left_ee_target = left_ee_task.transform_target_to_world
+        left_ee_target.translation[1] = 0.4
+        left_ee_target.translation[2] = 0.3 + 0.15 * 1# np.sin(2 * np.pi * 0.5 * t)  # Match the lift target's height
+
+        # viz.viewer["lift_target"].set_transform(lift_target.np)
+        viz.viewer["lift"].set_transform(configuration.get_transform_frame_to_world(lift_task.frame).np)
+
+        viz.viewer["ee_left_target"].set_transform(left_ee_target.np)
+        viz.viewer["ee_left"].set_transform(configuration.get_transform_frame_to_world(left_ee_task.frame).np)
+
+        velocity = solve_ik(configuration, tasks, dt, solver=solver)
+        configuration.integrate_inplace(velocity, dt)
+
+        viz.display(configuration.q)
+        rate.sleep()
+        t += dt
+
 
 if __name__ == "__main__":
     main()
