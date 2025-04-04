b3ea2049852deb9e55bd1b2f77941a8ea72cb51e
eneserciyes <m.eneserciyes@gmail.com>
2025-04-04 18:37:22 -0400

    better base and lift costs

--- robot/arm/mink_ik.py
+++ robot/arm/mink_ik.py
@@ -57,11 +57,11 @@ if __name__ == "__main__":
     # Also, keep the lift joint at a constant height.
     posture_cost = np.zeros((model.nv,))
     posture_cost[2] = 1e-3
-    posture_cost[model.joint("lift_joint").id] = 1e-3
+    posture_cost[3] = 1e-1 # lift joint
     posture_task = mink.PostureTask(model, cost=posture_cost)
 
     immobile_base_cost = np.zeros((model.nv,))
-    immobile_base_cost[:2] = 100
+    immobile_base_cost[:2] = 1000
     immobile_base_cost[2] = 1e-3
     damping_task = mink.DampingTask(model, immobile_base_cost)
 
