222ffcdd6eca82e763310ae73e4ce2e59785ab8d
GRAIL NUC 1067 <robotlab-infra@nyu.edu>
2025-04-07 16:11:51 -0400

    sending messages - enabling - motor commands

--- robot/arm/piper_sdk/can_utils.py
+++ robot/arm/piper_sdk/can_utils.py
@@ -1,3 +1,18 @@
+import struct
+import ctypes
+
+KP_MIN = 0.0
+KP_MAX = 500.0
+KD_MIN = -5.0
+KD_MAX = 5.0
+POS_MIN = -12.5
+POS_MAX = 12.5
+VEL_MIN = -18.0
+VEL_MAX = 18.0
+T_MIN = -18.0
+T_MAX = 18.0
+
+
 def can_data_to_uint8(data: bytearray):
     value = int.from_bytes(data, byteorder="big")
     if not (0 <= value <= 255):
@@ -32,3 +47,31 @@ def can_data_to_uint16(data: bytearray):
         print("Input value exceeds the range [0, 65535]")
     value &= 0xFFFF
     return value
+
+
+## Convert to bytes
+
+
+def uint16_to_bytes(value: int):
+    if not (0 <= value <= 65535):
+        raise OverflowError("Input value exceeds the range [0, 65535]")
+    return list(struct.unpack("BB", struct.pack(">H", value)))
+
+
+def int16_to_bytes(value: int):
+    if not (-32768 <= value <= 32767):
+        raise OverflowError("Input value exceeds the range [-32768, 32767]")
+    value = ctypes.c_int16(value).value
+    return list(struct.unpack("BB", struct.pack(">h", value)))
+
+
+def uint8_to_bytes(value: int):
+    if not (0 <= value <= 255):
+        raise OverflowError("Input value exceeds the range [0, 255]")
+    return list(struct.unpack("B", struct.pack(">B", value)))
+
+
+def float_to_uint(x: float, x_min: float, x_max: float, bits: int):
+    span: float = x_max - x_min
+    offset: float = x_min
+    return int((x - offset) * (float((1 << bits) - 1)) / span)
--- robot/arm/piper_sdk/hardware.py
+++ robot/arm/piper_sdk/hardware.py
@@ -4,7 +4,23 @@ import numpy as np
 import can
 from can.message import Message
 
-from robot.arm.piper_sdk.can_utils import can_data_to_int32, can_data_to_int16, can_data_to_uint16, can_data_to_uint8
+from robot.arm.piper_sdk.can_utils import (
+    can_data_to_int32,
+    can_data_to_int16,
+    can_data_to_uint16,
+    can_data_to_uint8,
+    float_to_uint,
+    POS_MIN,
+    POS_MAX,
+    VEL_MIN,
+    VEL_MAX,
+    KP_MIN,
+    KP_MAX,
+    KD_MIN,
+    KD_MAX,
+    T_MIN,
+    T_MAX,
+)
 
 RAD_TO_DEG = 57.295779513082320876798154814105
 DEG_TO_RAD = 0.017453292519943295769236907684886
@@ -16,6 +32,7 @@ TORQUE_COEFF = [1.18125, 1.18125, 1.18125, 0.95844, 0.95844, 0.95844]
 class PiperHardwareStation:
     def __init__(self, channel="can0") -> None:
         try:
+            self.channel_name = channel
             self.bus = can.interface.Bus(channel=channel, bustype="socketcan")
         except can.CanError as e:
             print(f"Failed to initialize CAN bus: {e}")
@@ -59,6 +76,14 @@ class PiperHardwareStation:
         else:
             print("CAN bus is not OK, skipping message read")
 
+    def send(self, can_id, data):
+        msg = Message(channel=self.channel_name, arbitration_id=can_id, data=data, dlc=8, is_extended_id=False)
+        if self.bus.state == can.BusState.ACTIVE:
+            try:
+                self.bus.send(msg)
+            except can.CanError:
+                print(can.CanError, "message not sent")
+
     def can_receive_frame(self, rx_message: Message):
         can_id = rx_message.arbitration_id
         can_data = rx_message.data
@@ -78,3 +103,38 @@ class PiperHardwareStation:
             with self._state_lock:
                 motor_id = can_id - 0x261
                 self.foc_status[motor_id] = can_data_to_uint8(can_data[5:6])
+
+    def enable_motors(self):
+        self.send(0x471, [0x07, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def disable_motors(self):
+        self.send(0x471, [0x07, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+    def set_ctrl_mode(self, ctrl_mode: int, move_mode: int, speed_rate: int, mit_mode: int):
+        self.send(0x151, [ctrl_mode, move_mode, speed_rate, mit_mode, 0x00, 0x00, 0x00, 0x00])
+
+    def send_motor_cmd(self, motor_id: int, pos: float, vel: float, kp: float, kd: float, t: float):
+        if not (0 <= motor_id <= 5):
+            print(f"Invalid motor ID: {motor_id}")
+            return
+
+        data = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+        pos_ref = float_to_uint(pos, POS_MIN, POS_MAX, 16)
+        vel_ref = float_to_uint(vel, VEL_MIN, VEL_MAX, 12)
+        kp_int = float_to_uint(kp, KP_MIN, KP_MAX, 12)
+        kd_int = float_to_uint(kd, KD_MIN, KD_MAX, 12)
+        t_ref = float_to_uint(t, T_MIN, T_MAX, 8)
+
+        data[0] = (pos_ref >> 8) & 0xFF # High byte
+        data[1] = pos_ref & 0xFF        # Low byte
+        data[2] = (vel_ref >> 4) & 0xFF
+        data[3] = (((vel_ref & 0xF) << 4) & 0xF0) | ((kp_int >> 8) & 0x0F)
+        data[4] = kp_int & 0xFF
+        data[5] = (kd_int >> 4) & 0xFF
+        data[6] = (((kd_int & 0xF) << 4) & 0xF0) | ((t_ref >> 4) & 0x0F)
+
+        crc = (data[0] ^ data[1] ^ data[2] ^ data[3] ^ data[4] ^ data[5] ^ data[6]) & 0x0F
+        data[7] = ((t_ref << 4) & 0xF0) | crc
+
+        self.send(0x15A + motor_id, data)
