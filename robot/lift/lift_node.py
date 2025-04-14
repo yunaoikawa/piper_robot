808a5a89047ffc4fdc598b091bc1001707d9d5f2
eneserciyes <m.eneserciyes@gmail.com>
2025-04-14 14:44:31 -0400

    lift test added

--- robot/lift/lift_node.py
+++ robot/lift/lift_node.py
@@ -1,9 +1,11 @@
 import os
+import time
 from typing import cast
 
 os.environ["CTR_TARGET"] = "Hardware"
 import phoenix6
 from phoenix6 import configs, controls, hardware
+from loop_rate_limiters import RateLimiter
 
 from robot.base.constants import CONTROL_FREQ
 
@@ -16,15 +18,23 @@ class Lift:
         self.position_signal = self.lift_motor.get_position()
         self.velocity_signal = self.lift_motor.get_velocity()
         self.status_signals = cast(list[phoenix6.BaseStatusSignal], [self.position_signal, self.velocity_signal])
-        self.position_request = controls.DynamicMotionMagicTorqueCurrentFOC(0, 20, 10, 100)
+        self.position_request = controls.DynamicMotionMagicTorqueCurrentFOC(0, 20, 10, 100, slot=0)
+        self.velocity_request = controls.VelocityTorqueCurrentFOC(0, slot=1)
         self.neutral_request = controls.NeutralOut()
 
         self.lift_motor_cfg = configs.TalonFXConfiguration()
+        # position control gains
         self.lift_motor_cfg.slot0.k_p = 50
         self.lift_motor_cfg.slot0.k_i = 0.0
         self.lift_motor_cfg.slot0.k_d = 0.0
         # self.lift_motor_cfg.slot0.k_s = 10
         # self.lift_motor_cfg.slot0.static_feedforward_sign = signals.StaticFeedforwardSignValue.USE_CLOSED_LOOP_SIGN
+
+        # velocity control gains
+        self.lift_motor_cfg.slot1.k_p = 0.0
+        self.lift_motor_cfg.slot1.k_i = 0.0
+        self.lift_motor_cfg.slot1.k_d = 0.0
+
         self.lift_motor_cfg.torque_current.peak_forward_torque_current = 120
         self.lift_motor_cfg.torque_current.peak_reverse_torque_current = -120
         self.lift_motor_cfg.audio.beep_on_boot = False
@@ -56,5 +66,24 @@ class Lift:
     def set_control(self, control: float) -> None:
         self.lift_motor.set_control(self.position_request.with_position(-control / 0.004))
 
+    def set_velocity_control(self, velocity: float) -> None:
+        self.lift_motor.set_control(self.velocity_request.with_velocity(-velocity / 0.004))
+
     def update_state(self):
         phoenix6.BaseStatusSignal.refresh_all(self.status_signals)
+
+
+if __name__ == "__main__":
+    rate = RateLimiter(100)
+    lift = Lift()
+    lift.set_neutral()
+    input("Press Enter to start")
+    try:
+        while True:
+            phoenix6.unmanaged.feed_enable(0.1)
+            lift.set_control(0.1)
+            rate.sleep()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        lift.set_neutral()
