75595636cb56e3df83fb30974c090d6efbe2881d
eneserciyes <m.eneserciyes@gmail.com>
2025-05-07 18:27:29 -0400

    increase kp to 10

--- robot/arm/arm_node.py
+++ robot/arm/arm_node.py
@@ -42,7 +42,7 @@ class ArmNode:
         self.controller_config = ControllerConfigFactory.get_instance().get_config("joint_controller")
         self.robot_config.urdf_path = urdf_path
         self.controller_config.controller_dt = 0.005
-        self.controller_config.default_kp = np.array([5.0, 5.0, 5.0, 5.0, 5.0, 5.0])
+        self.controller_config.default_kp = np.array([10.0, 10.0, 10.0, 10.0, 10.0, 10.0])
         self.controller_config.default_kd = np.array([0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
         self.piper = PiperJointController(self.robot_config, self.controller_config, self.can_port)
         self.target: mink.SE3 | None = None
