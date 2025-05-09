df8d41cdeebd07bad984ba90491363ed41ccc513
eneserciyes <m.eneserciyes@gmail.com>
2025-05-09 15:26:13 -0400

    comment out old stuff, add homing function

--- robot/arm/arm_node.py
+++ robot/arm/arm_node.py
@@ -1,46 +1,59 @@
 import time
 import numpy as np
-from typing import Any
+# from typing import Any
 from pathlib import Path
-import argparse
+# import argparse
 
 from piperlib import PiperJointController, RobotConfigFactory, ControllerConfigFactory, JointState
 import mink
-from mink import SE3
+# from mink import SE3
 # from dora import Node
 
 from robot.arm.mink_ik_arm import ArmIK
-from robot.msgs.pose import Pose, ArmCommand
+# from robot.msgs.pose import Pose, ArmCommand
 from robot.arm.fps_counter import FPSCounter
 
-class SE3Filter:
-    def __init__(self, alpha):
-        self.alpha = alpha
-        self.y: SE3 = None
-        self.is_init = False
 
-    def next(self, x):
-        if not self.is_init:
-            self.y = x
-            self.is_init = True
-            return self.y.copy()
-        self.y = self.y.interpolate(x, self.alpha)
-        return self.y.copy()
+# class SE3Filter:
+#     def __init__(self, alpha):
+#         self.alpha = alpha
+#         self.y: SE3 = None
+#         self.is_init = False
 
-    def reset(self):
-        self.y = None
-        self.is_init = False
+#     def next(self, x):
+#         if not self.is_init:
+#             self.y = x
+#             self.is_init = True
+#             return self.y.copy()
+#         self.y = self.y.interpolate(x, self.alpha)
+#         return self.y.copy()
+
+#     def reset(self):
+#         self.y = None
+#         self.is_init = False
+
+GRIPPER_OPEN = -22.0
 
 class ArmNode:
-    def __init__(self, can_port: str, mjcf_path: str, urdf_path: str, solver_dt: float = 0.01):
+    def __init__(
+        self, can_port: str, mjcf_path: str | None = None, urdf_path: str | None = None, solver_dt: float = 0.01
+    ):
+        _HERE = Path(__file__).parent
         self.can_port = can_port
-        self.mjcf_path = mjcf_path
+        if mjcf_path is None:
+            self.mjcf_path = (_HERE / "mujoco/scene_piper.xml").as_posix()
+        else:
+            self.mjcf_path = mjcf_path
+        if urdf_path is None:
+            self.urdf_path = (_HERE / "urdf/piper_description_left.xml").as_posix()
+        else:
+            self.urdf_path = urdf_path
         self.solver_dt = solver_dt
 
         # initialize arm
         self.robot_config = RobotConfigFactory.get_instance().get_config("piper")
         self.controller_config = ControllerConfigFactory.get_instance().get_config("joint_controller")
-        self.robot_config.urdf_path = urdf_path
+        self.robot_config.urdf_path = self.urdf_path
         self.controller_config.controller_dt = 0.005
         self.controller_config.default_kp = np.array([10.0, 10.0, 10.0, 10.0, 10.0, 10.0])
         self.controller_config.default_kd = np.array([0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
@@ -48,10 +61,11 @@ class ArmNode:
         self.target: mink.SE3 | None = None
         self.gripper_target: float | None = None
         self.target_timestamp: int | None = None
-        self.ik_solver = ArmIK(mjcf_path, solver_dt=self.solver_dt)
+        self.ik_solver = ArmIK(self.mjcf_path, solver_dt=self.solver_dt)
+        self.home_q = np.array([0.0, 1.58065, -0.578175, 0.0, -0.912, 0.78])
 
         # position low-pass filter
-        self.se3_filter = SE3Filter(0.4)
+        # self.se3_filter = SE3Filter(0.4)
 
         # fps counter
         self.ik_solver_fps_counter = FPSCounter("ik_solver")
@@ -66,12 +80,12 @@ class ArmNode:
         # self.piper.home_gripper()
 
         # home
-        q = np.array([0.0, 1.58065, -0.578175, 0.0, -0.912, 0.78]) # np.array(self.ik_solver.get_home_q())
+        q = self.home_q.copy()
         print(f"q_home: {np.round(q, 4)}")
         cmd = JointState(self.robot_config.joint_dof)
         cmd.timestamp = self.piper.get_timestamp() + 1.0
         cmd.pos = q
-        cmd.gripper_pos = -22.0
+        cmd.gripper_pos = 1.0 * GRIPPER_OPEN
         self.piper.set_joint_cmd(cmd)
         time.sleep(2.0)
         q = self.piper.get_joint_state().pos
@@ -87,15 +101,23 @@ class ArmNode:
             return False
         return True
 
-    def arm_command_handler(self, event: dict[str, Any]):
-        arm_command = ArmCommand.decode(event["value"], event["metadata"])
-        self.target = mink.SE3(arm_command.wxyz_xyz)
-        qd = self.ik_solver.solve_ik(self.target)
+    # def arm_command_handler(self, event: dict[str, Any]):
+    #     arm_command = ArmCommand.decode(event["value"], event["metadata"])
+    #     self.target = mink.SE3(arm_command.wxyz_xyz)
+    #     qd = self.ik_solver.solve_ik(self.target)
+    #     cmd = JointState(self.robot_config.joint_dof)
+    #     cmd.pos = qd
+    #     if arm_command.gripper is not None:
+    #         cmd.gripper_pos = arm_command.gripper * GRIPPER_OPEN
+    #     cmd.timestamp = self.piper.get_timestamp() + 0.1
+    #     self.piper.set_joint_cmd(cmd)
+
+    def home(self, gripper_target: float = 1.0):
+        q = self.home_q.copy()
         cmd = JointState(self.robot_config.joint_dof)
-        cmd.pos = qd
-        if arm_command.gripper is not None:
-            cmd.gripper_pos = arm_command.gripper
-        cmd.timestamp = self.piper.get_timestamp() + 0.1
+        cmd.timestamp = self.piper.get_timestamp() + 1.0
+        cmd.pos = q
+        cmd.gripper_pos = gripper_target * GRIPPER_OPEN
         self.piper.set_joint_cmd(cmd)
 
     def set_ee_target(self, ee_target: mink.SE3, gripper_target: float | None = None, preview_time: float = 0.1):
@@ -104,7 +126,7 @@ class ArmNode:
         cmd = JointState(self.robot_config.joint_dof)
         cmd.pos = qd
         if gripper_target is not None:
-            cmd.gripper_pos = gripper_target
+            cmd.gripper_pos = gripper_target * GRIPPER_OPEN
         cmd.timestamp = self.piper.get_timestamp() + preview_time
         self.piper.set_joint_cmd(cmd)
 
@@ -112,12 +134,12 @@ class ArmNode:
         self.update_joint_positions()
         return self.ik_solver.forward_kinematics()
 
-    def step(self):
-        self.update_joint_positions()
-        ee_pose = self.ik_solver.forward_kinematics()  # update current joint positions
+    # def step(self):
+    #     self.update_joint_positions()
+    #     ee_pose = self.ik_solver.forward_kinematics()  # update current joint positions
 
-        pose_msg = Pose(time.perf_counter_ns(), ee_pose.wxyz_xyz)
-        # self.node.send_output("ee_pose", *pose_msg.encode())
+    #     pose_msg = Pose(time.perf_counter_ns(), ee_pose.wxyz_xyz)
+    #     # self.node.send_output("ee_pose", *pose_msg.encode())
 
     def update_joint_positions(self):
         q = self.piper.get_joint_state().pos
@@ -127,36 +149,35 @@ class ArmNode:
         print("called stop")
         pass
 
-    def spin(self):
-        for event in self.node:
-            event_type = event["type"]
-            if event_type == "INPUT":
-                event_id = event["id"]
+    # def spin(self):
+    #     for event in self.node:
+    #         event_type = event["type"]
+    #         if event_type == "INPUT":
+    #             event_id = event["id"]
 
-                if event_id == "arm_command":
-                    self.arm_command_handler(event)
+    #             if event_id == "arm_command":
+    #                 self.arm_command_handler(event)
 
-                elif event_id == "tick":
-                    self.step()
+    #             elif event_id == "tick":
+    #                 self.step()
 
-            elif event_type == "STOP":
-                self.stop()
+    #         elif event_type == "STOP":
+    #             self.stop()
 
 
-if __name__ == "__main__":
-    _HERE = Path(__file__).parent
+# if __name__ == "__main__":
+#     _HERE = Path(__file__).parent
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--mjcf_path", type=str, required=True)
+#     parser.add_argument("--urdf_path", type=str, required=True)
+#     parser.add_argument("--can_port", type=str, required=True)
+#     parser.add_argument("--solver_dt", type=float, required=True)
+#     args = parser.parse_args()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--mjcf_path", type=str, required=True)
-    parser.add_argument("--urdf_path", type=str, required=True)
-    parser.add_argument("--can_port", type=str, required=True)
-    parser.add_argument("--solver_dt", type=float, required=True)
-    args = parser.parse_args()
-
-    arm_node = ArmNode(
-        can_port=args.can_port,
-        mjcf_path=(_HERE / args.mjcf_path).as_posix(),
-        urdf_path=(_HERE / args.urdf_path).as_posix(),
-        solver_dt=args.solver_dt,
-    )
-    arm_node.spin()
+#     arm_node = ArmNode(
+#         can_port=args.can_port,
+#         mjcf_path=(_HERE / args.mjcf_path).as_posix(),
+#         urdf_path=(_HERE / args.urdf_path).as_posix(),
+#         solver_dt=args.solver_dt,
+#     )
+#     arm_node.spin()
