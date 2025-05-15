4d934600fc95838cf75d0f708e38c3343e7b16e5
eneserciyes <m.eneserciyes@gmail.com>
2025-05-15 17:41:38 -0400

    update joystick to use the Base class instead of publishing

--- robot/teleop/joystick.py
+++ robot/teleop/joystick.py
@@ -7,10 +7,12 @@ import time
 import numpy as np
 import pygame
 from pygame.joystick import Joystick
+from loop_rate_limiters import RateLimiter
+# from dora import RateLimiter
 
-from dora import Node
-
-from robot.msgs.base_command import BaseCommand, CommandType
+# from robot.msgs.base_command import BaseCommand, CommandType
+# from robot.base.base_node import CommandType
+from robot.base.base_node import Base
 
 XBOX_CONTROLLER_MAP = {
     "start": 7,
@@ -46,66 +48,62 @@ class JoystickNode:
         self.max_vels = [np.array([0.5, 0.5, 1.57]), np.array([0.25, 0.25, 1.57]), np.array([0.75, 0.75, 1.57])]
         self.max_vel_setting = 0
         self.control_loop_running = False
-        self.node = Node()
+        self.base = Base()
+        self.base.start_control()
+        # self.node = Node()
 
-    def step(self):
-        pygame.event.pump()
-        if not self.control_loop_running and self.joystick.get_button(controller_map["start"]):
-            self.control_loop_running = True
-            print("Control started")
+    def control_loop(self):
+        rate = RateLimiter(60)
+        while True:
+            pygame.event.pump()
+            if not self.control_loop_running and self.joystick.get_button(controller_map["start"]):
+                self.control_loop_running = True
+                print("Control started")
 
-        if self.control_loop_running and self.joystick.get_button(controller_map["back"]):
-            self.control_loop_running = False
-            print("Control stopped")
+            if self.control_loop_running and self.joystick.get_button(controller_map["back"]):
+                self.control_loop_running = False
+                print("Control stopped")
 
-        if self.control_loop_running:
-            left_bumper = self.joystick.get_button(controller_map["l1"])
-            if left_bumper:
-                self.max_vel_setting = (self.max_vel_setting + 1) % 3
-                print("Max velocity setting: ", self.max_vel_setting)
-                time.sleep(0.1)
+            if self.control_loop_running:
+                left_bumper = self.joystick.get_button(controller_map["l1"])
+                if left_bumper:
+                    self.max_vel_setting = (self.max_vel_setting + 1) % 3
+                    print("Max velocity setting: ", self.max_vel_setting)
+                    time.sleep(0.1)
 
-            vy = -self.joystick.get_axis(controller_map["right_horizontal_axis"])  # Right analog stick
-            vx = -self.joystick.get_axis(controller_map["right_vertical_axis"])  # Right analog stick
-            w = -self.joystick.get_axis(controller_map["left_horizontal_axis"])  # Left analog stick
-            target_velocity = np.array([vx, vy, w])
-            target_velocity = apply_deadzone(target_velocity)
+                vy = -self.joystick.get_axis(controller_map["right_horizontal_axis"])  # Right analog stick
+                vx = -self.joystick.get_axis(controller_map["right_vertical_axis"])  # Right analog stick
+                w = -self.joystick.get_axis(controller_map["left_horizontal_axis"])  # Left analog stick
+                target_velocity = np.array([vx, vy, w])
+                target_velocity = apply_deadzone(target_velocity)
 
-            target_velocity = self.max_vels[self.max_vel_setting] * target_velocity
-            lift_target = self.joystick.get_hat(0)[1]  # pad Y axis
-            if sum(np.abs(target_velocity)) > 0.0:
-                base_command = BaseCommand(
-                    timestamp=time.perf_counter_ns(),
-                    type=CommandType.BASE_VELOCITY,
-                    target=target_velocity.ravel(),
-                )
-                self.node.send_output("base_command", *base_command.encode())
-            if lift_target != 0:
-                lift_command = BaseCommand(
-                    timestamp=time.perf_counter_ns(),
-                    type=CommandType.LIFT_POSITION,
-                    target=np.array([0.39 if lift_target > 0 else 0.0]),
-                )
-                self.node.send_output("base_command", *lift_command.encode())
+                target_velocity = self.max_vels[self.max_vel_setting] * target_velocity
+                lift_target = self.joystick.get_hat(0)[1]  # pad Y axis
+                if sum(np.abs(target_velocity)) > 0.0:
+                    self.base.set_target_base_velocity(target_velocity)
+                if lift_target != 0:
+                    self.base.set_target_lift(np.array([0.39 if lift_target > 0 else 0.0]))
 
-    def stop(self):
-        pass
+            rate.sleep()
 
-    def spin(self):
-        for event in self.node:
-            event_type = event["type"]
-            if event_type == "INPUT":
-                event_id = event["id"]
-                if event_id == "tick":
-                    self.step()
-            elif event_type == "STOP":
-                self.stop()
+    # def stop(self):
+    #     pass
+
+    # def spin(self):
+    #     for event in self.node:
+    #         event_type = event["type"]
+    #         if event_type == "INPUT":
+    #             event_id = event["id"]
+    #             if event_id == "tick":
+    #                 self.step()
+    #         elif event_type == "STOP":
+    #             self.stop()
 
 
 def main():
     pygame.init()
     node = JoystickNode()
-    node.spin()
+    node.control_loop()
 
 if __name__ == "__main__":
     main()
